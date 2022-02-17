ef0d27c6bdd52b57d08440a97b4c1539d99fe1e1
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-02-17 17:48:50 -0300

    Estrutura da Aplicação

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import * as C from './App.styles'
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Minhas Finanças</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        ...
+      </C.Body>
+    </C.Container>
+  )
+}
+
+export default App
