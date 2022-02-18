9d8005123e7c09c8d5719547ec26fe581f4d01f8
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-02-18 11:50:04 -0300

    Dados iniciais para teste

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    {
+        date: new Date(2022, 1, 17),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12
+    },
+    {
+        date: new Date(2022, 1, 17),
+        category: 'food',
+        title: 'Burger King',
+        value: 28.00
+    },
+    {
+        date: new Date(2022, 1, 17),
+        category: 'Rent',
+        title: 'Aluguel apt',
+        value: 2300
+    },
+    {
+        date: new Date(2022, 2, 17),
+        category: 'Salary',
+        title: 'Salário jx',
+        value: 4500
+    },
+]
